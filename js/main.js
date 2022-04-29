5fbb3f1cb592daf14eb1f586dbf750570219dc9d
Igor Pluznikov <igplu@dfds.com>
2022-04-29 10:37:39 +0200

    add popup

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-function trackRatingClick(rating) {
+function trackRatingClick(rating, elem) {
+  window.dataLayer.push({
+    event: "feedbackGiven",
+    grade: rating,
+  });
 
-    window.dataLayer.push({
-        'event': 'feedbackGiven', 
-        'grade': rating
-    });
+  mydiv = document.getElementsByClassName("ratingIcon");
+  for (i = 0; i < mydiv.length; i++) {
+    elem.style.opacity = 0.5;
+    setTimeout(() => {
+      elem.style.opacity = 1;
+    }, 100);
+  }
+
+  const popup = document.getElementsByClassName("hover_bkgr_fricc")[0];
+  popup.style.display = "block";
+  setTimeout(() => {
+    popup.style.display = "none";
+  }, 1500);
 }
